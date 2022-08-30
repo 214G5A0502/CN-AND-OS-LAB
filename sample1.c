d6f9a1b5ba9e4bb0be5496ada358542416fe344c
anil5a <112237622+anil5a@users.noreply.github.com>
2022-08-30 11:53:59 +0530

    anil1.c

---
+++ sample1.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Anil kumar
+   Roll No: 214G5A0502
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
