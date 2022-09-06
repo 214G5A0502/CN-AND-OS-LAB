4e836e1ed376c4c7f83ba31abca3335fa53909b7
214G5A0502 <112237622+214G5A0502@users.noreply.github.com>
2022-09-06 12:50:33 +0530

    M.ANIL KUMAR Created RB.c

---
+++ EXP1/RB.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Anil kumar
+   Roll No: 214G5A0502
+   Title of the Experiment: CPU Scheduling Using Round Robin Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
