4a7f06d41ce58ed424dc59652edd7315b2e756ba
214G5A0502 <112237622+214G5A0502@users.noreply.github.com>
2022-09-13 12:46:28 +0530

    M.Anil kumar completed SJF.C with c source code

--- EXP1/SJF.C
+++ EXP1/SJF.C
@@ -5,8 +5,8 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-#include<conio.h>
-main()
+//#include<conio.h>
+int main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
@@ -44,4 +44,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-getch();}
+//getch();
+}
