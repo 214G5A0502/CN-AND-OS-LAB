fba1fce123f7f45cfdaf09b9ca4eeacf53057936
214G5A0502 <214G5A0502@srit.ac.in>
2022-10-11 12:36:30 +0530

    FIRST EXECUTION RESULTS:

--- EXP1/RB.c
+++ EXP1/RB.c
@@ -5,11 +5,10 @@
    Date of Execution: 06-09-2022
 */
 #include<stdio.h>
-main()
+int main()
 {
 int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
 float awt=0,att=0,temp=0;
-clrscr();
 printf("Enter the no of processes -- ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -46,5 +45,4 @@ printf("\nThe Average Waiting time is -- %f ",awt/n);
 printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\t%d \t %d \t\t %d \t\t %d \n",i+1,ct[i],wa[i],tat[i]);
-//getch();
-}
+return 0;}
